e3014bd9edfcd01a7b62ce95905c8fc37c2aac74
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-23 22:34:39 -0500

    Recipe-list component layout and recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import {RecipeItemComponent} from "./recipe-item/recipe-item.component";
+import {Recipe} from "../recipe.model";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
   imports: [
-    RecipeItemComponent
+    RecipeItemComponent,
+    NgForOf
   ],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1513104890138-7c749659a591')
+  ];
 
 }
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,9 @@
+export class Recipe {
+  constructor(
+    public id: string | bigint,
+    public name: string,
+    public description: string,
+    public imagePath: string
+  ) {
+  }
+}
