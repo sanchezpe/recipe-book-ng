6688dab23c71f65c3cacbf429808577d64e38fe8
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-23 22:34:41 -0500

    Display components conditionally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,15 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     HeaderComponent,
     RecipesComponent,
     ShoppingListComponent,
+    CommonModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'recipe-book-ng';
+  loadedFeature = 'recipes';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
