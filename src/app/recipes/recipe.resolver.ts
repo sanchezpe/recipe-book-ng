20e2e492e3b6f09bbc31ba52cdd7c5cb7cbb6b0a
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-23 22:34:54 -0500

    Fix recipe resolver on edit mode

--- src/app/recipes/recipe.resolver.ts
+++ src/app/recipes/recipe.resolver.ts
@@ -2,8 +2,15 @@ import { ResolveFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 export const recipeResolver: ResolveFn<Recipe[]> = (route, state) => {
   let dataStorageService = inject(DataStorageService);
-  return dataStorageService.fetchRecipes();
+  let recipeService = inject(RecipeService);
+  let recipes = recipeService.getRecipes();
+  if (recipes.length == 0) {
+    return dataStorageService.fetchRecipes();
+  } else {
+    return recipes;
+  }
 };
