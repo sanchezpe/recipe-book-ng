86b1c778d7c63c8f1d9e4710ac34178a8acbc9ed
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-23 22:34:46 -0500

    Add /recipes child route navigation

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -2,20 +2,18 @@ import { Component, Input } from '@angular/core';
 import { NgForOf } from '@angular/common';
 import { Recipe } from '../../recipe.model';
 import { RecipeService } from '../../recipe.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-item',
   standalone: true,
-  imports: [NgForOf],
+  imports: [NgForOf, RouterLink],
   templateUrl: './recipe-item.component.html',
   styleUrl: './recipe-item.component.css',
 })
 export class RecipeItemComponent {
   @Input() recipe!: Recipe;
+  @Input() index!: number;
 
   constructor(private recipeService: RecipeService) {}
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 }
