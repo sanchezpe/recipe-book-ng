fdb9462c1bdc9c4ba8989188d7bd9424610012f2
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-23 22:34:44 -0500

    Add ingredients to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { DropdownDirective } from '../../shared/dropdown.directive';
 import { Recipe } from '../recipe.model';
 import { NgForOf } from '@angular/common';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,5 +12,11 @@ import { NgForOf } from '@angular/common';
   styleUrl: './recipe-detail.component.css',
 })
 export class RecipeDetailComponent {
-  @Input() recipe?: Recipe;
+  @Input() recipe!: Recipe;
+
+  constructor(private recipeService: RecipeService) {}
+
+  addToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable({
   providedIn: 'root',
@@ -41,9 +42,13 @@ export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() {}
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
