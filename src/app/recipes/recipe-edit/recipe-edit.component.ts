36fdd001e21e4432799f38d8e5195015f477b61f
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-23 22:34:46 -0500

    Create recipe-edit component

---
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-edit.component.html',
+  styleUrl: './recipe-edit.component.css',
+})
+export class RecipeEditComponent {}
