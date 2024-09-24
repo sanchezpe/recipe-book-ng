a004e133731e8ab67f55ce98ec487614c1166d16
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-23 22:34:40 -0500

    Shopping-list component databinding

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ShoppingEditComponent} from "./shopping-edit/shopping-edit.component";
+import {Ingredient} from "../shared/ingredient.model";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
   imports: [
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    NgForOf
   ],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomatoes', 5),
+    new Ingredient('Cheese', 1),
+  ]
 
 }
