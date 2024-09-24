2357e144cca8807215eb854b6c49ebdb71b7748d
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-23 22:34:48 -0500

    NgForm - template approach in shopping-edit component

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,23 +1,21 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './shopping-edit.component.html',
   styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
-  @ViewChild('nameInput') nameInputRef?: ElementRef;
-  @ViewChild('amountInput') amountInput?: ElementRef;
-
   constructor(private shoppingListService: ShoppingListService) {}
 
-  onAddItem() {
-    let ingredientName = this.nameInputRef?.nativeElement.value;
-    let ingredientAmount = this.amountInput?.nativeElement.value;
+  onAddItem(form: NgForm) {
+    let ingredientName = form.value.name;
+    let ingredientAmount = form.value.amount;
     this.shoppingListService.addIngredient(
       new Ingredient(ingredientName, ingredientAmount),
     );
