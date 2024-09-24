a5ee28447680554a659600603039a0d8aa64e17c
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-23 22:34:56 -0500

    Add load spinner and error handling in auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from './auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error?: string;
 
   constructor(private authService: AuthService) {}
 
@@ -25,12 +28,20 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
     if (this.isLoginMode) {
       // TODO
     } else {
       this.authService.signUp(email, password).subscribe({
-        next: (resData) => console.log(resData),
-        error: (error) => console.log(error),
+        next: (resData) => {
+          console.log(resData);
+          this.isLoading = false;
+        },
+        error: (error) => {
+          console.log(error);
+          this.error = 'An error occurred';
+          this.isLoading = false;
+        },
       });
     }
 
