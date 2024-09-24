dbec34018068ef6194886a1a2136b836acac156f
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-23 22:34:44 -0500

    Add ingredients to recipe

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { DropdownDirective } from '../../shared/dropdown.directive';
 import { Recipe } from '../recipe.model';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-detail',
   standalone: true,
-  imports: [DropdownDirective],
+  imports: [DropdownDirective, NgForOf],
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css',
 })
