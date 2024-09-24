4c643d17cf8905c687b049aad51f942dcc6d5326
Pablo Sánchez <27013095+sanchezpe@users.noreply.github.com>
2024-09-23 22:34:46 -0500

    Mark active /recipes child routes

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -2,12 +2,12 @@ import { Component, Input } from '@angular/core';
 import { NgForOf } from '@angular/common';
 import { Recipe } from '../../recipe.model';
 import { RecipeService } from '../../recipe.service';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-item',
   standalone: true,
-  imports: [NgForOf, RouterLink],
+  imports: [NgForOf, RouterLink, RouterLinkActive],
   templateUrl: './recipe-item.component.html',
   styleUrl: './recipe-item.component.css',
 })
